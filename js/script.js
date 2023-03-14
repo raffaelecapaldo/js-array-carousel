688d5ed5f196e556b43db55c45164d24cc47fd67
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-14 15:52:03 +0100

    first tag img of thumbs and slides default=on

--- js/script.js
+++ js/script.js
@@ -4,13 +4,26 @@ const images = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img
 
 const carousel = document.getElementById("carousel");
 const thumbs = document.getElementById("thumbs");
+const buttonUp = document.getElementById("button-up");
+const buttondown = document.getElementById("button-down");
+let currentIndex = 0;
 
-function createCarousel() {
+
+
+function createCarousel() { //ciclo for per creare i tag img sia nelle slides che nelle thumbs
     for (let i = 0; i < images.length; i++) {
-        carousel.innerHTML += `<img src="${images[i]}" alt="">`
-        thumbs.innerHTML += `<img src="${images[i]}" alt="">`
+        carousel.innerHTML += `<img class="image-slide" src="${images[i]}" alt="">`
+        thumbs.innerHTML += `<img class="image-slide" src="${images[i]}" alt="">`
     }
+//attiva tutti i primi tag img
+document.querySelectorAll(".image-slide")[currentIndex].classList.add("active");
+document.querySelectorAll(".image-slide")[currentIndex + images.length].classList.add("active");
+}
+
+
+function goNext() {
 
 }
 
 createCarousel();
+
