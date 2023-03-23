031ace95609270eeba8441dd4269cdc4abe26c7b
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-23 16:08:19 +0100

    Autoplay starts when users zone out of carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 //Creiamo array di objects con le info sulle immagini del carosello
-
-const images = [ {
+const images = [{
     file: "01.webp",
     title: "Spiderman",
 },
@@ -21,36 +20,28 @@ const images = [ {
     title: "Marvel Avengers",
 },
 
-
-
 ]
-
 const carousel = document.getElementById("carousel");
 const thumbs = document.getElementById("thumbs");
 const buttonUp = document.getElementById("button-up");
 const buttonDown = document.getElementById("button-down");
 let currentIndex = 0;
-
 buttonUp.addEventListener("click", goPrev);
 buttonDown.addEventListener("click", goNext);
-
 
 function createCarousel() { //ciclo for each per creare carosello coi dati dell'array di oggetti
     images.forEach((image) => {
         carousel.innerHTML += `<div class="slide"> <img class="image-slide" src="img/${image.file}" alt="${image.title}"> <div class="titlebox">${image.title}</div></div>`
         thumbs.innerHTML += `<img class="image-slide" src="img/${image.file}" alt="${image.title}">`
-
     })
     //attiva tutti i primi tag img
     document.querySelectorAll(".slide")[currentIndex].classList.add("active");
     document.querySelectorAll(".image-slide")[currentIndex + images.length].classList.add("active");
 }
 
-
 function goNext() {
     document.querySelectorAll('.slide')[currentIndex].classList.remove("active");//rimuovi active a chi lo possiede attualmente
     document.querySelectorAll('.image-slide')[currentIndex + images.length].classList.remove("active");
-
     if (currentIndex === images.length - 1) {// se il numero supera la quantità massima delle immagini disponibili, ritorna a zero
         currentIndex = 0;
     }
@@ -59,15 +50,12 @@ function goNext() {
     }
     document.querySelectorAll('.slide')[currentIndex].classList.add("active");//assegno ora dove ti trovi (se avanti o a zero) active
     document.querySelectorAll('.image-slide')[currentIndex + images.length].classList.add("active");
-
 }
-
 
 function goPrev() {// come sopra ma vai indietro
     document.querySelectorAll('.image-slide')[currentIndex].classList.remove("active");
     document.querySelectorAll('.image-slide')[currentIndex + images.length].classList.remove("active");
     document.querySelectorAll('.titlebox')[currentIndex + images.length].classList.remove("active");
-
 
     if (currentIndex === 0) {//se sei arrivato alla prima foto, riparti dall'ultima, usa lenght per avere la loro quantità
         currentIndex = images.length - 1;
@@ -79,27 +67,30 @@ function goPrev() {// come sopra ma vai indietro
     document.querySelectorAll('.image-slide')[currentIndex + images.length].classList.add("active");
 }
 
-
 createCarousel();
-
 
 //BONUS MEOW - ho imparato cos'è una funzione ricorsiva
 const sound = new Audio('sounds/stray.mp3'); // elemento audio con miagolio Stray
-
 function checkVariable() {
     if (currentIndex == images.indexOf("img/04.webp")) { //controlla currentIndex sia attualmente alla posizione della foto di Stray nell'indice
         sound.play(1); // riproduci il suono
         setTimeout(checkVariable, 15000);// riproduci il suono tra altri 15 secondi se rimani fermo sulla foto, permetti di riprodurre di nuovo il suono soltanto
         // dopo 15sec se ritorni sulla foto da altra posizione
-
     } else {
         setTimeout(checkVariable, 100); // ogni 100 secondi che l'incide non corrisponde, richiama la funzione
     }
 }
-
 checkVariable();//primo avvio della funzione
 
 //AUTOPLAY ogni 2 sec
-const autoplay = setInterval(goNext, 2000);
-//Quando il mouse va sul carosello, interrompi l'autoplay
-carousel.addEventListener("mouseover", () => clearInterval(autoplay));
+const autoplay = () => timer = setInterval(goNext, 2000)
+const autoplayzones = document.querySelectorAll(".autoplay-zone");
+autoplayzones.forEach((zone) => {
+    //Quando il mouse va sul carosello o le thumbs, interrompi l'autoplay
+    zone.addEventListener("mouseover", () => clearInterval(timer));
+    //Quando il mouse esce dal carosello o le thumbs, ricomincia l'autoplay
+    zone.addEventListener("mouseout", autoplay);
+})
+
+//Autoplay inizia a pagina caricata
+autoplay();
